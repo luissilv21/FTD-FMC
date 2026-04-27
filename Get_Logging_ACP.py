7935a3aeae27e0be8b56116e002c2e8800e224a1
luissilv21 <102678197+luissilv21@users.noreply.github.com>
2026-04-27 16:03:53 -0400

    Update Get_Logging_ACP.py

--- Get_Logging_ACP.py
+++ Get_Logging_ACP.py
@@ -6,9 +6,9 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # FMC connection details
-FMC_HOST = "10.122.51.26"
-USERNAME = "luis_api"
-PASSWORD = "C1scoRocks!"
+FMC_HOST = "x.x.x.x"
+USERNAME = "api"
+PASSWORD = "your_password"
 
 def get_auth_token():
     """
